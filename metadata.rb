35e7a76783e4a637e66791ea894583a495d7dcdd
Mark Workman <mworkman@shelterinsurance.com>
2015-02-02 14:39:30 -0600

    inital setup

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'rabbitmq'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures rabbitmq'
+long_description 'Installs/Configures rabbitmq'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: rabbitmq
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
